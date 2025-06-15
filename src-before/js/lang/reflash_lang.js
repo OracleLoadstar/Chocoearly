cde1bbefee3c3f7019d941c607acefd5f65d1b87
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-06-15 20:38:08 +0800

    update

--- src-before/js/lang/reflash_lang.js
+++
@@ -1,9 +0,0 @@
-
-function reflash_langguage() {
-    langchange_arr.forEach(element => {
-        let langElement = document.getElementById(element);
-        if (langElement) {
-            langElement.textContent = lang[element];
-        }
-    });
-}
